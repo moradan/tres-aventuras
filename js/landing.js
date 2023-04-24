e465f641b0e033b96c188752ea5a35afcb35788c
moradan <realmoradan@hotmail.com>
2023-04-24 12:43:52 -0300

    control del resaltado de menu;

--- js/landing.js
+++ js/landing.js
@@ -30,3 +30,27 @@ function navegar(e) {
 
     objetivo.scrollIntoView();
 }
+
+/**
+ * Esta funciona normalmente apuntaria a los nav-link dentro de los nav-item; sin embargo en esta pagina use la clase 'secundario' en el nav-item para resaltar el link activo. Esta funcion esta modificada para listar los nav-item en vez de los nav-link. Y cambie el uso de la clase active (propia de bootstrap) por 'secundario' propia de mi hoja de estilos.
+ */
+function activarMenu() {
+    const REM = 16;
+    const DESPLAZADO = 5 * REM;
+    
+    const li = document.querySelectorAll(".nav-item");
+    const sec = document.querySelectorAll(".destino-nav");
+
+    let len = sec.length;
+
+
+    while(--len && window.scrollY + DESPLAZADO < sec[len].offsetTop) {}
+    li.forEach((link) => {link.classList.remove("secundario")});
+    
+    if (len > 0) {
+        li[len - 1].classList.add("secundario");
+    }
+}
+
+activarMenu();
+window.addEventListener("scroll", activarMenu);
